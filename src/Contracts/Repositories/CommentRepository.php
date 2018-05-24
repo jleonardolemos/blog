792315469a1277ec1292a34e8a46a9641aa9020b
Leonardo Lemos <leonardo@mixd.com.br>
2018-05-24 13:40:25 -0300

    add findByPostId method to CommentRepository

--- src/Contracts/Repositories/CommentRepository.php
+++ src/Contracts/Repositories/CommentRepository.php
@@ -7,4 +7,6 @@ use Illuminate\Support\Collection;
 interface CommentRepository extends Repository
 {
     public function findByUserId(int $userId) : Collection;
+
+    public function findByPostId(int $postId) : Collection;
 }
